8bb63bf72a8dd08b711b432eb46e9ab2434747f6
Vladislav Tsybulya <vladmir909@mail.ru>
2019-03-18 14:50:01 +0800

    Add Z2 and Z3

---
+++ Z2/Z2/Z2.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+char matrixCh[4][4] = {};
+int matrix[4][4] = {};
+ifstream in("input.txt");
+
+int main()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			in >> matrixCh[i][j];
+			//cout << matrixCh[i][j];
+		}
+		//cout << endl;
+	}
+
+	int i = 0, j = 0, k = 1;
+	matrix[i][j] = 1;
+	while (k != 16)
+	{
+		//cout << matrixCh[i][j] << endl;
+		switch (matrixCh[i][j])
+		{
+		case 'r':
+			j < 3 ? j++ : j = 0;
+			break;
+		case 'l':
+			j > 0 ? j-- : j = 3;
+			break;
+		case 'u':
+			i > 0 ? i-- : i = 3;
+			break;
+		case 'd':
+			i < 3 ? i++ : i = 0;
+		}
+		matrix[i][j]++;
+		/*for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				cout << matrix[i][j] << " ";
+			}
+			cout << endl;
+		}*/
+		if (matrix[i][j] == 2)
+		{
+			cout << "Cycle in point (" << j << ", " << i << ")";
+			return 0;
+		}
+		k++;
+	}
+	cout << "There is no cycle!";
+}
---
+++ Z3/Z3/Z3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int temp;
+	long long answer = 0;
+	long long j = 0;
+	while (n > 0)
+	{
+		temp = (n % 3);
+		answer += pow(3, j)*temp + pow(3, j + 1)*temp;
+		n /= 3;
+		j += 2;
+	}
+	cout << answer;
+}
